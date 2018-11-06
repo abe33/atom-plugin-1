50408549d0a60d652b644f4057ac7ae35c7dce5a
abe33 <cedric.nehemie@gmail.com>
2018-11-06 14:11:52 +0100

    Remove test focus

--- spec/json-runner-spec.js
+++ spec/json-runner-spec.js
@@ -25,7 +25,7 @@ walk(path.resolve(__dirname, 'json', 'expectations'), '.js', file => {
 
 const featureSet = require(featureSetPath());
 
-fdescribe('JSON tests', () => {
+describe('JSON tests', () => {
   beforeEach(() => {
     jasmine.useRealClock();
     const jasmineContent = document.querySelector('#jasmine-content');
